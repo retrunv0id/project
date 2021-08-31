ba9d2ebce349d2628467c9ff9e11a092c2e40388
hesoyam-oke <76074457+hesoyam-oke@users.noreply.github.com>
2021-09-01 00:29:36 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#         Premium Auto Script By Sugiono        #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
+echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
+echo -e "# Build Up By Kang Coly                         #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Thanks To                                     #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Jav Sexy                                      #" | lolcat
+echo -e "# XNXX                                          #" | lolcat
+echo -e "# XHAMSTER                                      #" | lolcat
+echo -e "# SEXTOP                                        #" | lolcat
+echo -e "# JAV HIHI                                      #" | lolcat
+echo -e "# XVIDEOS                                       #" | lolcat
+echo -e "# Coli Ngab Sampe Nyeri Kontolnya               #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "=================================================" | lolcat
